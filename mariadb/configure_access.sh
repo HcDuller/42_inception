f201f66aacab79f3343270cdc8340855aa23e1b9
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-05-02 15:57:59 -0300

    mariadb working_

--- mariadb/configure_access.sh
+++ mariadb/configure_access.sh
@@ -5,7 +5,7 @@ chown -R mysql:mysql /var/run/mysqld
 sleep 2
 mysql --user=root -e "CREATE USER 'admin'@'%' IDENTIFIED BY '1234';"
 mysql --user=root -e "CREATE DATABASE wordpress;"
-mysql --user=root -e"GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';"
-mysql --user=root -e"FLUSH PRIVILEGES;"
+mysql --user=root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';"
+mysql --user=root -e "FLUSH PRIVILEGES;"
 mariadb-admin shutdown
 #/usr/bin/mariadbd
