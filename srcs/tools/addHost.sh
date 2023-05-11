78215176045c139852813003abaa28a71187f187
Henrique de Campos Duller <henriqueduller@hotmail.com>
2023-05-11 17:00:39 -0300

    changing localhost to hde-camp.42.fr

--- srcs/tools/addHost.sh
+++ srcs/tools/addHost.sh
@@ -4,6 +4,6 @@ if grep -q "hde-camp" /etc/hosts; then
   echo "hde-camp already exists in /etc/hosts"
 else
   echo "Adding hde-camp to /etc/hosts"
-  echo "\n127.0.0.1	hde-camp" >> /etc/hosts
+  echo "\n127.0.0.1	hde-camp.42.fr" >> /etc/hosts
   sudo sed -i '/^$/d' /etc/hosts
 fi
